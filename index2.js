546b2bcf2c9f3f7eb50ad4f5eeb5d995dc398973
PhoenixNill <1270128439@qq.com>
2019-02-10 21:53:46 +0800

    2.8

--- index2.js
+++ index2.js
@@ -23,7 +23,27 @@ new Vue({
                     src: '6.jpg'
                 }
 
-            ]
+            ],
+            items: [
+                { title: 'Menuet D major' },
+                { title: '尘世城' },
+                { title: 'Clair de lune' },
+                { title: 'flaming' }
+            ],
+        }
+    }, computed: {
+        color() {
+            return (decodeURI(document.URL).split('&')[2]).split('=')[1];
+
+        }
+    },
+    methods: {
+        play() {
+            var cars = ["pause", "play_arrow"];
+            if (document.getElementById('K').innerHTML == cars[0])
+                document.getElementById('K').innerHTML = cars[1];
+            else
+                document.getElementById('K').innerHTML = cars[0];
         }
     }
 })
@@ -41,6 +61,10 @@ rest = parseInt(thisURL.split('&')[1].replace(/[^0-9]/ig, ""))
 var m = work
 var s = 0
 var K = rest
+var i = 0
+var j = 0
+var y = 0
+var z = 0
 var settime = setInterval(function () {
     showtime();
 }, 1000);
@@ -79,4 +103,24 @@ function backSetURL() {
     ipcRenderer.send('BackSet');
 
 }
-document.getElementById('rest').innerHTML = "休息时间" + K + "分"
+function NextSong() {
+    songlist = ["尘世城.mp3", "Clair De Lune.mp3", "Flaming.mp3"]
+    song = ["尘世城", "Clair de lune", "flaming"]
+    singers = ["LA TALE", "Claude Debussy", "郑成河"]
+    document.getElementById("music").src = songlist[i++ % 3]
+    document.getElementById("song").innerHTML = song[j++ % 3]
+    document.getElementById("singer").innerHTML = singers[y++ % 3]
+
+}
+document.getElementById('rest').innerHTML = "休息时间" + K + "分钟"
+const shell = require('electron').shell
+
+const exLinksBtn = document.getElementById('issue')
+const exLinksBtn2 = document.getElementById('about')
+
+exLinksBtn.addEventListener('click', function (event) {
+    shell.openExternal('https://github.com/PhoenixNil/realse_electron/issues')
+})
+exLinksBtn2.addEventListener('click', function (event) {
+    shell.openExternal('https://www.glasstower.top/')
+})
--- set.js
+++ set.js
@@ -1,8 +1,12 @@
-
 function toPage2() {
     worktime = document.getElementById("worktime").innerHTML
     resttime = document.getElementById("resttime").innerHTML
-    window.location.href = encodeURI("counttime.html?work=" + worktime + "&" + "rest=" + resttime);
+    passcolor = document.getElementById("hrefcolor").innerHTML
+    window.location.href = encodeURI("counttime.html?work=" + worktime + "&" + "rest=" + resttime + '&' + "color=" + passcolor);
+}
+function dislike() {
+    imgarray = ["bg11.jpg", "set2.jpg", "set3.jpg"]
+    document.getElementById("setimg").src = imgarray[i++]
 }
 new Vue({
     el: '#app',
@@ -16,17 +20,35 @@ new Vue({
             resttimes: [
                 { rest: '1minute' }, { rest: '3minutes' }, { rest: '10minutes' }, { rest: '15minutes' }, { rest: '30minutes' }, { rest: '45minutes' }, { rest: '1hour' }, { rest: '2hour' }, { rest: '3hour' }, { rest: '5hour' }
             ],
-            bottomNav: 3
+            bottomNav: 5,
+            love: 'grey',
+            i: Math.floor(Math.random() * 4),
+            imgs: ["bg11.jpg", "set2.jpg", "set3.jpg", "set4.jpg"]
+        }
+    },
+    methods: {
+        like: function () {
+            this.love = 'pink'
+        },
+        dislike: function () {
+            this.i++
+            if (this.i > 3) {
+                this.i = 0;
+            }
+            this.love = 'grey'
+
         }
     },
     computed: {
         color() {
             switch (this.bottomNav) {
-                case 0: return 'blue-grey'
+                case 0: return 'deep-orange'
                 case 1: return 'teal'
                 case 2: return 'grey'
                 case 3: return '#29B6F6'
                 case 4: return '#EEDD82'
+                case 5: return 'dark'
+                case 6: return 'purple'
             }
         }
     }
